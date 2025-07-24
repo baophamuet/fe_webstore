671b78cca3596a39dab7b2d6c1e646968e051bf8
baophamuet <baophamuet@gmail.com>
2025-07-24 21:46:33 +0700

    Hello world!

---
+++ src/views/App.js
@@ -0,0 +1,18 @@
+import logo from '../assets/logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world!!!
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
