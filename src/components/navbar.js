abbeb3ec368ff80530e69f7b3df60aadac270216
Pham The Bao <bao.phamthe@mobifone.vn>
2025-08-04 13:11:02 +0700

    Fix navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,23 +5,56 @@ import { NavLink } from "react-router-dom";
 import { FaSearch, FaUser, FaHeart, FaShoppingCart } from 'react-icons/fa';
 
 class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFixed: false,
+      navHeight: 0
+    };
+    this.navRef = React.createRef();
+  }
+
+  componentDidMount() {
+    if (this.navRef.current) {
+      this.setState({ navHeight: this.navRef.current.offsetHeight });
+    }
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const { navHeight, isFixed } = this.state;
+
+    if (window.scrollY > navHeight && !isFixed) {
+      this.setState({ isFixed: true });
+    } else if (window.scrollY <= navHeight && isFixed) {
+      this.setState({ isFixed: false });
+    }
+  };
+
   render() {
     return (
-      <div className="topnav">
-        {/* Logo section - Bên trái */}
+      <div
+        ref={this.navRef}
+        className={`topnav ${this.state.isFixed ? "fixed" : ""}`}
+      >
+        {/* Logo section */}
         <div className="logo-section">
           <img src={logo} className="App-logo" alt="logo" />
           <NavLink className="logo" to="/home">SHOPPINK</NavLink>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
 
-        {/* Search bar - Giữa (hoặc sát nav-links) */}
+        {/* Search bar */}
         <div className="search-bar">
           <FaSearch className="search-icon" />
           <input type="text" placeholder="Tìm kiếm" />
         </div>
 
-        {/* Nav links - Bên phải */}
+        {/* Nav links */}
         <div className="nav-links">
           <NavLink to="/home">Home</NavLink>
           <NavLink to="/helps">Help</NavLink>
